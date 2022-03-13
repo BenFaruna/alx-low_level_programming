35de149fe1cb51acf434c9a0b11187f067d056fe
Faruna Benjamin <benjaminfaruna@gmail.com>
2022-03-12 22:31:39 -0800

    __

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	printf("Size of a char: %ld bytes(s)", sizeof(char));
 	printf("Size of an int: %ld bytes(s)", sizeof(int));
-	printf("Size of a long int: %ld bytes(s)", sizeof(long));
-	printf("Size of a long long int: %ld bytes(s)", sizeof(unsigned long));
+	printf("Size of a long int: %ld bytes(s)", sizeof(unsigned long));
+	printf("Size of a long long int: %ld bytes(s)", sizeof(long long));
 	printf("Size of a float: %ld bytes(s)", sizeof(float));
-	
+
 	return (0);
 }
