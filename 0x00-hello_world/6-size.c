9b6c8799907275655640ed19777dbef2b106d08e
Faruna Benjamin <benjaminfaruna@gmail.com>
2022-03-12 22:47:22 -0800

    _____

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes(s)%c", sizeof(char), '\n');
-	printf("Size of an int: %ld bytes(s)%c", sizeof(int), '\n');
-	printf("Size of a long int: %ld bytes(s)%c", sizeof(unsigned long), '\n');
-	printf("Size of a long long int: %ld bytes(s)%c", sizeof(long long), '\n');
-	printf("Size of a float: %ld bytes(s)%c", sizeof(float), '\n');
+	printf("Size of a char: %ld byte(s)%c", sizeof(char), '\n');
+	printf("Size of an int: %ld byte(s)%c", sizeof(int), '\n');
+	printf("Size of a long int: %ld byte(s)%c", sizeof(unsigned long), '\n');
+	printf("Size of a long long int: %ld byte(s)%c", sizeof(long long), '\n');
+	printf("Size of a float: %ld byte(s)%c", sizeof(float), '\n');
 
 	return (0);
 }
