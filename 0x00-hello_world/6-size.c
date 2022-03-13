be9c8194bc92fd2d56c2a58c54498b5089bc45ff
Faruna Benjamin <benjaminfaruna@gmail.com>
2022-03-12 22:03:42 -0800

    Completed madatory tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function printing out the values of various data types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)", sizeof(char));
+	printf("Size of an int: %d bytes(s)", sizeof(int));
+	printf("Size of a char: %d bytes(s)", sizeof(long));
+	printf("Size of a char: %d bytes(s)", sizeof(unsigned long);
+	printf("Size of a char: %d bytes(s)", sizeof(float));
+}
