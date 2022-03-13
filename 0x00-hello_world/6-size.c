5a0d655d6e570ada204a2e761874127283c7e685
Faruna Benjamin <benjaminfaruna@gmail.com>
2022-03-12 22:10:05 -0800

    Aded correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes(s)", sizeof(char));
-	printf("Size of an int: %ld bytes(s)", sizeof(int));
-	printf("Size of a char: %ld bytes(s)", sizeof(long));
-	printf("Size of a char: %ld bytes(s)", sizeof(unsigned long));
-	printf("Size of a char: %ld bytes(s)", sizeof(float));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(long));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(unsigned long));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(float));
+	
+	return (0);
 }
