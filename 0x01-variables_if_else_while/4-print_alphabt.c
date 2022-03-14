f427c3c732eb16a338d416666327e497329dab53
Faruna Benjamin <benjaminfaruna@gmail.com>
2022-03-14 09:15:20 -0700

    4-print_alphabt.c completed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets except 'e' and 'q'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (alpha[i] == 'e' || alpha[i] == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(alpha[i]);
+		}
+	}
+	putchar('\n');
+}
