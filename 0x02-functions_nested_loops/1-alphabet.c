fd267cedabfe179143d2c18d7d04bcaad60bac72
BenFaruna <benjaminfaruna@gmail.com>
2022-03-16 07:53:36 +0100

    Added betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include <unistd.h>
 
+/**
+ * print_alphabet - print letters of the alphabet a-z
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
-        int i = 0;
-        char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
 
-        while (i < 26)
-        {
-            write(1, &ch[i], 1);
-            i++;
-        }
-        write(1, "\n", 1);
+	while (i < 26)
+	{
+		write(1, &ch[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
