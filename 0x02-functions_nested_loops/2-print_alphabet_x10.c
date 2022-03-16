3281b9efb040e4839782643cc7249b876122680f
BenFaruna <benjaminfaruna@gmail.com>
2022-03-16 08:16:10 +0100

    Completed 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - print letters of the alphabet a-z 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		int i = 0;
+		char ch[] = "abcdefghijklmnopqrstuvwxyz";
+
+		while (i < 26)
+		{
+			write(1, &ch[i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
+}
