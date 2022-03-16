4f17d681746cb33bcba9bcb159df0ab240444f8c
BenFaruna <benjaminfaruna@gmail.com>
2022-03-16 14:09:04 +0100

    Completed 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+/**
+ * _islower - check if a char is lower case
+ *
+ * Return: 1 for true and 0 when false
+ */
+int _islower(int c)
+{
+	int result;
+
+	result = (c >=  97 && c <= 122) ? 1 : 0;
+
+	return result;
+}
