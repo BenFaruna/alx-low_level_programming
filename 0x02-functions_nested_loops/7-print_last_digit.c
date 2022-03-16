f0fd1718ea3d38b2e91c3dc2f8fba8e1eeb5965c
BenFaruna <benjaminfaruna@gmail.com>
2022-03-17 00:55:49 +0100

    Having trouble with the output of 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,25 +10,20 @@ int _putchar(char c);
 int print_last_digit(int n)
 {
 	int result;
-	int temp;
 
-	if (n > 99)
+	if (n < 0)
 	{
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 100) % 10 + '0');
+		n = n * -1;
 	}
-	else if (n >= 0)
+
+	if (n > 100000)
 	{
-		result = n % 10;
-		temp = result + '0';
+		n = n / 100000;
 	}
-	else
-	{
-		n = -1 * n;
-		result = n % 10;
-		temp = result + '0';
-	}
-	_putchar(temp);
+
+	result = n % 10;
+
+	_putchar(result + '0');
 
 	return (result);
 }
