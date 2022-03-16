fcfb65f10527daeabb81a58cee74f1fb006ec5a9
BenFaruna <benjaminfaruna@gmail.com>
2022-03-16 08:22:08 +0100

    Completed 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * print_alphabet - print letters of the alphabet a-z 10 times
+ * print_alphabet_x10 - print letters of the alphabet a-z 10 times
  *
  * Return: void
  */
