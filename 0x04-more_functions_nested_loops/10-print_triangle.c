f4c670db3a67962a163c8da01cf345aee717124c
BenFaruna <benjaminfaruna@gmail.com>
2022-03-18 05:40:22 +0100

    finished mandatory task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - print a square from a parameter
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int temp;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	temp=0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j >= (size - temp))
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		temp++;
+		_putchar('\n');
+	}
+}
