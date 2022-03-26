9e88f4d78fd15a405f514947a841dc9cde579d25
BenFaruna <benjaminfaruna@gmail.com>
2022-03-27 00:10:05 +0100

    Reverse string completed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,5 +11,27 @@
  */
 void rev_string(char *s)
 {
-	/*printf("%s", rev_string);*/
+	int i;
+	int l;
+	char *begin_ptr;
+	char *end_ptr;
+	char ch;
+
+	l = strlen(s);
+
+	begin_ptr = s;
+	end_ptr = s;
+
+	for (i = 0; i < l - 1; i++)
+		end_ptr++;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		ch = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = ch;
+
+		begin_ptr++;
+		end_ptr--;
+	}
 }
