8e55822f8648fb1882f3fcb6221a89b728bd883b
BenFaruna <benjaminfaruna@gmail.com>
2022-04-10 15:48:13 +0100

    argc and argv quiz almost completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}
