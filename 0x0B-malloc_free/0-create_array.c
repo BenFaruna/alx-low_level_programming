1834762c18759984718fbabbec92c857bebd9ed3
BenFaruna <benjaminfaruna@gmail.com>
2022-04-13 00:25:29 +0100

    Corrected segmentation error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	char_array = malloc(sizeof(unsigned int) * size);
+	char_array = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		char_array[i] = c;
