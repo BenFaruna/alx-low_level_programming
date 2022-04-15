de671f01f4796ee8247f10882524c5db8e77e44a
BenFaruna <benjaminfaruna@gmail.com>
2022-04-15 15:29:54 +0100

    2-calloc completed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *mem = malloc(nmemb * size);
 
-	if (mem == NULL || nmemb == NULL || size == NULL)
+	if (mem == NULL)
+		return (NULL);
+	
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		mem[i] = "";
+		mem[i] = '0';
 
 	return (mem);
 }
