1eab222474100a774c2baa3e0da32c807d4b6a0d
BenFaruna <benjaminfaruna@gmail.com>
2022-04-17 23:03:14 +0100

    Betty coding style for 0-print_nanme.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,15 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - prints string using function pointer
+ *
+ * @name: the string to be printed
+ * @f: function pointer used for printing the name
+ */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
