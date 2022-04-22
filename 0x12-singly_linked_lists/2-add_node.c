9de3aecbf16dd4dedf157849cda94a23fcd3de95
BenFaruna <benjaminfaruna@gmail.com>
2022-04-22 03:42:25 +0100

    add node amd list count function completed

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return(NULL);
+
+	if (head == NULL)
+		new_node->next = NULL;
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
+}
