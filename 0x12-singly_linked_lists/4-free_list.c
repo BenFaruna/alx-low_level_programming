b60cce9c84e059919c766763516e817afb782b47
BenFaruna <benjaminfaruna@gmail.com>
2022-04-22 05:42:20 +0100

    4-free_list.c betty style completed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_list - clears the memory space allocated for a linked list
+ *
+ * @head: the start of the linked list
+ *
+ * Return: Nothing
+ */
 void free_list(list_t *head)
 {
 	list_t *next;
