3f9d71ac21f252e290f69f44e70a4e2bef8bbbda
BenFaruna <benjaminfaruna@gmail.com>
2022-04-22 05:38:41 +0100

    Betty style completed for 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,14 +6,16 @@
  * add_node - add node to the beginning of list
  *
  * @head: the first element of the list
- * @char: the element to be addded
+ * @str: the element to be addded
+ *
+ * Return: the head node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
 	if (head == NULL)
 		new_node->next = NULL;
