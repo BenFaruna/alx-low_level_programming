a6bf7f44e79e0bf3ed4d5382cc3d7f58e6677f00
BenFaruna <benjaminfaruna@gmail.com>
2022-05-02 18:40:44 +0100

    __

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include "main.h"
 
 /**
@@ -11,19 +12,34 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int fd_write;
 	unsigned int s_len = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-	if (fd == -1)
-		return (-1);
+	if (fd < 0)
+	{
+		if (errno == EEXIST)
+		{
+			fd = open(filename, O_WRONLY | O_TRUNC);
+			if (fd == -1)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
 
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 
-	write(fd, text_content, s_len);
+	fd_write = write(fd, text_content, s_len);
+
+	if (fd_write == -1)
+		return (-1);
+
+	close(fd);
 	return (1);
 }
