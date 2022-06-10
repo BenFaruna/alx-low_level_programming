0392e6e4fa00f52c687c8cd8c0fb3a36a50743f4
BenFaruna <benjaminfaruna@gmail.com>
2022-06-10 02:22:03 +0100

    Started doubly linked list mandatory task

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *next;
+	struct dlistint_s *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif /* LISTS_H */
